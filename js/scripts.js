893e227a45df0075ff0d29cfa12bb7c5bfee9fb4
Gabriel Rocha <rochagbriel@gmail.com>
2023-03-20 21:10:51 +0100

    Add: Key id in all Objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
 let pokemonList = [
     {
+        id: 1,
         name:'Bulbasaur',
         height: 7,
         types: ['grass', 'poison']
     },
     {
+        id: 4,
         name:'Charmander',
         height: 6,
         types: ['fire', 'normal']
     },
     {
+        id: 7,
         name:'Squirtle',
         height: 5,
         types: ['water', 'normal']
