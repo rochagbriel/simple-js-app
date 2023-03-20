6a50277df6db1bb044e960ea028428c4ce4cfeda
Gabriel Rocha <rochagbriel@gmail.com>
2023-03-20 20:58:47 +0100

    Added an Array of Objects named pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello World!');
-
-let favoriteFood = 'Hamburger';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name:'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name:'Charmander',
+        height: 6,
+        types: ['fire', 'normal']
+    },
+    {
+        name:'Squirtle',
+        height: 5,
+        types: ['water', 'normal']
+    }
+];
