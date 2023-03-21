27fc49fbf03dbfb48faf23d038c3151cfc1e514d
Gabriel Rocha <rochagbriel@gmail.com>
2023-03-21 14:44:11 +0100

    Add: A loop that print in html paragraphs the name and height for each object

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,24 @@ const pokemonList = [
     {
         id: 1,
         name:'Bulbasaur',
-        height: 7,
+        height: 0.7,
         types: ['grass', 'poison']
     },
     {
         id: 4,
         name:'Charmander',
-        height: 6,
+        height: 0.6,
         types: ['fire', 'normal']
     },
     {
         id: 7,
         name:'Squirtle',
-        height: 5,
+        height: 0.5,
         types: ['water', 'normal']
     }
 ];
+
+// this loop prints the Pokemon name and size in individual html paragraphs
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<p>'+ pokemonList[i].name + '(' + 'height:' + pokemonList[i].height + ')' + '</p>') 
+}
